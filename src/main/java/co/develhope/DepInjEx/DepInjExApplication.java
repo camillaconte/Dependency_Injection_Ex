0b4c543a27e1c188167e85bc5898537aa73a2455
camillaconte <118888603+camillaconte@users.noreply.github.com>
2023-03-12 21:45:26 +0100

    first commit

---
+++ src/main/java/co/develhope/DepInjEx/DepInjExApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.DepInjEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepInjExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepInjExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/develhope/DepInjEx/DepInjExApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.DepInjEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepInjExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
